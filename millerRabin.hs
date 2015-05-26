36576bf221347d1008de6c5a3a689b38d8605ca2
h3nnn4n <uber.renan@gmail.com>
2015-05-26 19:49:04 +0000

    wip

--- millerRabin.hs
+++ millerRabin.hs
@@ -1,8 +1,15 @@
 module MillerRabin where
 
-isPrime :: Integer n -> int k -> Bool
-isPrime  n k
-    | n == 2                = True
-    | n `mod` 2 == 0        = False
-    | otherwise             = do ws <- witnesses n k 
-                                 return $ and [ test 
+factor2 :: Integer -> (Integer, Integer)
+factor2 n = fN n 0
+    where
+    fN n s | even n    = fN (n `div` 2) (s + 1)
+           | otherwise = (n,s)
+
+congruente :: Integer -> Integer -> Integer -> Bool
+congruente p q n
+    | q - p `mod` n == 0    = True
+    | otherwise             = False
+
+iter :: Integer -> (Integer, Integer) -> [Integer]
+iter n (m, k) = map (\x -> m^2^x) [1, 2 .. k]
