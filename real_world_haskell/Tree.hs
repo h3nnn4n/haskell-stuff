bf8be4d91d9f1f61d87a5377ff6a14f9e3419af1
h3nnn4n <uber.renan@gmail.com>
2015-10-15 23:39:25 -0300

    Added go{R,L}, offset, rotate{L,R}, balanced insert/delete, build a balanced tree from a list and removed old tree samples

--- real_world_haskell/Tree.hs
+++ real_world_haskell/Tree.hs
@@ -5,6 +5,22 @@ data Tree a = Node a (Tree a) (Tree a)
 --height :: (Num a, Ord a) => Tree t -> a
 height Empty = 0
 height (Node a b c) = 1 + max (height b) (height c)
+--
+--offset :: (Num a, Ord a) => Tree t -> a
+offset Empty = 0
+offset (Node a b c) = height b - height c
+
+--checkOffset :: (Num b, Ord b) => Tree a -> [b]
+checkOffset Empty        = []
+checkOffset (Node a b c) = offset (Node a b c) : checkOffset b ++ checkOffset c
+
+--goL :: Tree a -> Tree a
+goL Empty = Empty
+goL (Node a b c) = b
+
+--goR :: Tree a -> Tree a
+goR Empty = Empty
+goR (Node a b c) = c
 
 --find :: Ord a => a -> Tree a -> Bool
 find x Empty = False
@@ -32,19 +48,56 @@ delete x (Node a b c)
     | x >  a     = Node a b (delete x c)
     | x == a     = deleteFind (Node a b c)
         where deleteFind (Node a' Empty c') = c'
-              deleteFind (Node a' b' c') = Node w wz c'
+              deleteFind (Node a' b' c')    = Node w wz c'
                 where (w, wz) = deleteBiggest b'
+
+--insert :: Ord a => a -> Tree a -> Tree a
+insertBalanced x Empty = Node x Empty Empty
+insertBalanced x (Node a b c)
+    | x == a    = Node a b c
+    | x <  a    = avl $ Node a (insertBalanced x b) c
+    | x >  a    = avl $ Node a b (insertBalanced x c)
+
+--delete :: Ord a => a -> Tree a -> Tree a
+deleteBalanced x Empty   = Empty
+deleteBalanced x (Node a b c)
+    | x <  a     = avl $ Node a (delete x b) c
+    | x >  a     = avl $ Node a b (delete x c)
+    | x == a     = deleteFind (Node a b c)
+        where deleteFind (Node a' Empty c') = c'
+              deleteFind (Node a' b' c')    = avl $ Node w wz c'
+                where (w, wz) = deleteBiggest b'
+
+--avl :: Tree a -> Tree a
+avl Empty = Empty
+avl (Node a l r)
+    | abs( offsetNode ) < 2               = Node a l r
+    | offsetNode ==  2 && offsetR /= -1   = rotateR (Node a l r)
+    | offsetNode ==  2 && offsetR == -1   = rotateR (Node a (rotateL l) r)
+    | offsetNode == -2 && offsetR /=  1   = rotateL (Node a l r)
+    | offsetNode == -2 && offsetR ==  1   = rotateL (Node a (rotateR l) r)
+    where
+      offsetNode = offset (Node a l r)
+      offsetR    = offset r
+      offsetl    = offset l
+
+--rotateR :: Tree a -> Tree a
+rotateR Empty = Empty
+rotateR (Node a (Node b l r) r')  = Node b l (Node a r r')
+rotateR (Node a l r) = Node a l r
+
+--rotateL :: Tree a -> Tree a
+rotateL Empty = Empty
+rotateL (Node a l (Node b l' r')) = Node a (Node b l l') r'
+rotateL (Node a l r) = Node a l r
+
+--buildFromList' :: Ord a => [a] -> Tree a
+buildFromList' []     = Empty
+buildFromList' [x]    = Node x Empty Empty
+buildFromList' (x:xs) = insertBalanced x (buildFromList' xs)
 
 --buildFromList :: Ord a => [a] -> Tree a
 buildFromList []     = Empty
 buildFromList [x]    = Node x Empty Empty
 buildFromList (x:xs) = insert x (buildFromList xs)
-
-tree0 = Empty
-tree1 = Node 1 (Empty) (Empty)
-tree2 = Node 2 (Node 1 Empty Empty) (Empty)
-tree3 = Node 2 (Node 1 Empty Empty) (Node 3 Empty Empty)
-tree4 = Node 2 (Node 1 Empty Empty) (Node 3 Empty (Node 4 Empty Empty))
-tree5 = Node 2 (Node 1 Empty Empty) (Node 3 Empty (Node 4 Empty (Node 5 Empty Empty)))
-tree6 = Node 2 (Node 1 Empty Empty) (Node 3 Empty (Node 4 Empty (Node 6 (Node 5 Empty Empty) Empty)))
 
